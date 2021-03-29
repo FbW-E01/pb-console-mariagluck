00b9632dc3acd96390c4b2306f95fef81cd33905
mariagluck <mariamart.berlin@gmail.com>
2021-03-29 13:49:36 +0200

    first JS test

---
+++ starting.js
@@ -0,0 +1,21 @@
+const texto ="this is an exercise in console logging"
+console.log(texto)
+
+let message = "this is the variable"
+console.log(message)
+
+let firstName = "Maria"
+console.log(firstName)
+
+let secondName = "Martinez Santos"
+console.log (firstName, secondName)
+
+
+let Name = "Wonder"
+let surname = "Woman"
+let job = "super heroine"
+let city = "Themyscira – Also known as Paradise Island"
+let archEnemy = " Cheetah"
+console.log (Name, surname, job, city, archEnemy)
+
+console.log("this is a " + job, "famously known as " + Name, surname, "coming from " + city, "and her arch enemy is " + archEnemy);
